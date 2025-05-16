1c2a846608cbfbeec6a11da7931f2a4c438d8669
Christian Vase Frederiksen <chf004@edu.zealand.dk>
2025-05-16 10:24:55 +0200

    Lavet activity klasse

--- Dyreinternat_Library/Models/Activity.cs
+++ Dyreinternat_Library/Models/Activity.cs
@@ -1,7 +1,68 @@
-﻿namespace Dyreinternat_Library
+﻿using System.Xml.Linq;
+
+namespace Dyreinternat_Library
 {
-    public class Class1
+    public class Activity
     {
+        private string _titel;
+        private string _description;
+        private string _datetime;
+        private int _numberOfPerticipants;
+        private string _author;
+        private int _activityID;
+
+
+        public string Titel
+        {
+            get => _titel;
+            set => _titel = value;
+        }
+
+        public string Description
+        {
+            get => _description;
+            set => _description = value;
+        }
+        public string DateTime 
+        {
+           get => _datetime;
+           set => _datetime = value;
+        
+        }
+
+        public int NumberOfPerticipants 
+        { 
+            get => _numberOfPerticipants;
+            set => _numberOfPerticipants = value;
+        
+        }
+
+        public string Author
+        {
+            get => _author;
+            set => _author = value;
+        }
+
+        public int ActivityID
+        {
+            get => _activityID;
+            set => _activityID = value;
+        }
+
+        public Activity(string titel, string description, string datetime, int numberOfPerticipants, string author, int activityID) 
+        { 
+        
+            Titel = titel;
+            Description = description;
+            DateTime = datetime;
+            NumberOfPerticipants = numberOfPerticipants;
+            Author = author;
+            ActivityID = activityID;
+        
+        
+        
+        }
+
 
     }
 }
