4fbbdbf85a9b27ae315c273c8bde4c6c6f7d77db
VisualizedConfusion2 <thh005.edu.zealand.dk>
2025-05-16 10:05:38 +0200

    flere classes

--- Dyreinternat_Library/Models/Booking.cs
+++ Dyreinternat_Library/Models/Booking.cs
@@ -1,7 +1,33 @@
 ﻿namespace Dyreinternat_Library
 {
-    public class Class1
+    public class Booking
     {
+        private string phoneNumber;
+        private int bookingID;
 
+
+        public string PhoneNumber
+        {
+            get
+            {
+                return phoneNumber;
+            }
+            set => phoneNumber = value;
+        }
+
+        public int BookingID 
+        { 
+            get
+            {
+                return bookingID;
+            }
+            set => bookingID = value; 
+        }
+
+        public Booking(string phoneNumber, int bookingID)
+        {
+            PhoneNumber = phoneNumber;
+            BookingID = bookingID;
+        }
     }
 }
