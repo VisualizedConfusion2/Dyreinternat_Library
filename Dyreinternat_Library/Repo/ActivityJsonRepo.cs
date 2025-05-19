f9da3a0189d21412abb94be009d27918bc8f8728
VisualizedConfusion2 <thh005.edu.zealand.dk>
2025-05-19 11:48:39 +0200

    repo activity

--- Dyreinternat_Library/Repo/ActivityJsonRepo.cs
+++ Dyreinternat_Library/Repo/ActivityJsonRepo.cs
@@ -1,13 +1,60 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
+using System.Text.Json;
+using Dyreinternat_Library.Interfaces;
 
 namespace Dyreinternat_Library.Repo
 {
-    public class ActivityJsonRepo
+    public class ActivityJsonRepo : IActivityRepo
     {
-        private list
+        private List<Activity> _activities = new List<Activity>(); // List of all Activities
+        private string _path; // Filepath for the json file
+
+        public ActivityJsonRepo(string path)
+        {
+            _path = path;
+            LoadFile(_path);
+        }
+
+        private void LoadFile(string path) // Method to get the data from the json file
+        {
+            string file = path + "activities.json";
+            if (File.Exists(file))
+            {
+                string json = File.ReadAllText(file);
+                var activities = JsonSerializer.Deserialize<List<Activity>>(json);
+                if (activities != null)
+                    _activities = activities;
+            }
+        }
+
+        private void SaveFile(string path) // Method to save the data to the json file
+        {
+            File.WriteAllText(path + "activities.json", JsonSerializer.Serialize(_activities, new JsonSerializerOptions { WriteIndented = true }));
+        }
+
+        public void Add(Activity activity) // Adds an activity to the repository
+        {
+            _activities.Add(activity);
+            SaveFile(_path);
+        }
+
+        public List<Activity> GetAll() // Returns the list of all activities
+        {
+            return _activities;
+        }
+
+        public Activity GetByID(int id) // Returns an activity from a specific id
+        {
+            foreach (Activity activity in _activities)
+            {
+                if (id == activity.ActivityID)
+                {
+                    return activity;
+                }
+            }
+            return null;
+        }
     }
 }
