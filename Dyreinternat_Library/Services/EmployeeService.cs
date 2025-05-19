30c559f62d4b5b8e6d31bdf633297d015ee22e61
VisualizedConfusion2 <thh005.edu.zealand.dk>
2025-05-19 12:23:38 +0200

    services for employee

--- Dyreinternat_Library/Services/EmployeeService.cs
+++ Dyreinternat_Library/Services/EmployeeService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Dyreinternat_Library.Interfaces;
+using Dyreinternat_Library.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,26 @@ namespace Dyreinternat_Library.Services
 {
     public class EmployeeService
     {
+        private IEmployeeRepo _employeeRepo;
+        public EmployeeService(IEmployeeRepo employeeRepo)
+        {
+            _employeeRepo = employeeRepo;
+        }
+
+        public List<Employee> GetAll()
+        {
+            return _employeeRepo.GetAll();
+        }
+
+        public void Add(Employee Employee)
+        {
+            _employeeRepo.Add(Employee);
+        }
+
+
+        public Employee GetByID(int id)
+        {
+            return _employeeRepo.GetByID(id);
+        }
     }
 }
