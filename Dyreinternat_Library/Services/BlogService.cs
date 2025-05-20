ba583b97367f55c7fdbceca1acc919b6dc234def
markushugo <markuslynggaard@gmail.com>
2025-05-20 10:30:51 +0200

    Blog

--- Dyreinternat_Library/Services/BlogService.cs
+++ Dyreinternat_Library/Services/BlogService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Dyreinternat_Library.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,26 @@ namespace Dyreinternat_Library.Services
 {
     public class BlogService
     {
+        private IBlogRepo _blogRepo;
+        public BlogService(IBlogRepo blogRepo)
+        {
+            _blogRepo = blogRepo;
+        }
+
+        public List<Blog> GetAll()
+        {
+            return _blogRepo.GetAll();
+        }
+
+        public void Add(Blog blog)
+        {
+            _blogRepo.Add(Blog);
+        }
+
+
+        public Blog GetByID(int blogID)
+        {
+            return _blogRepo.GetByID(blogID);
+        }
     }
 }
