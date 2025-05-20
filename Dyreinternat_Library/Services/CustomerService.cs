52f426d4deafbaa3f557951f92a782a4906542af
markushugo <markuslynggaard@gmail.com>
2025-05-20 10:16:29 +0200

    Customer og DoctorVisit Service

--- Dyreinternat_Library/Services/CustomerService.cs
+++ Dyreinternat_Library/Services/CustomerService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Dyreinternat_Library.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,26 @@ namespace Dyreinternat_Library.Services
 {
     public class CustomerService
     {
+        private ICustomerRepo _customerRepo;
+        public CustomerService(ICustomerRepo customerRepo)
+        {
+            _customerRepo = customerRepo;
+        }
+
+        public List<Customer> GetAll()
+        {
+            return _customerRepo.GetAll();
+        }
+
+        public void Add(Customer Customer)
+        {
+            _customerRepo.Add(Customer);
+        }
+
+
+        public Customer GetByID(int customerID)
+        {
+            return _customerRepo.GetByID(customerID);
+        }
     }
 }
--- Dyreinternat_Library/Services/DoctorVisitService.cs
+++ Dyreinternat_Library/Services/DoctorVisitService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Dyreinternat_Library.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,26 @@ namespace Dyreinternat_Library.Services
 {
     public class DoctorVisitService
     {
+        private IDoctorVisitRepo _doctorVisitRepo;
+        public DoctorVisitService(IDoctorVisitRepo doctorVisitRepo)
+        {
+            _doctorVisitRepo = doctorVisitRepo;
+        }
+
+        public List<DoctorVisit> GetAll()
+        {
+            return _doctorVisitRepo.GetAll();
+        }
+
+        public void Add(DoctorVisit DoctorVisit)
+        {
+            _doctorVisitRepo.Add(DoctorVisit);
+        }
+
+
+        public DoctorVisit GetByID(int createdByEmployeeID)
+        {
+            return _doctorVisitRepo.GetByID(createdByEmployeeID);
+        }
     }
 }
